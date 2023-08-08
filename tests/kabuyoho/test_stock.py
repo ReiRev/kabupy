cbfaf02d3f92d382e1bae104d08b76ad1e988ff8
ReiRev <reirev2913@gmail.com>
2023-08-09 03:35:47 +0900

    fix: change directory settings and add an environment variable

--- tests/kabuyoho/test_stock.py
+++ tests/kabuyoho/test_stock.py
@@ -3,6 +3,7 @@ import requests_mock
 from money import Money
 
 import kabupy
+import os
 
 
 class TestStock:
@@ -11,7 +12,12 @@ class TestStock:
         [(3260, Money("692", "JPY")), (5210, Money("1192", "JPY"))],
     )
     def test_stock_price(self, helpers, security_code, price):
-        text = helpers.html2text(f"/workspaces/kabupy/tests/kabuyoho/html/{security_code}.html")
+        text = helpers.html2text(
+            filename=os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                f"html/{security_code}.html",
+            )
+        )
         with requests_mock.Mocker() as m:
             m.get(f"https://kabuyoho.jp/sp/reportTop?bcode={security_code}", text=text)
             assert kabupy.kabuyoho.stock(security_code).price == price
@@ -21,7 +27,12 @@ class TestStock:
         [(3260, 1_200_000_000), (5210, 13_300_000_000)],
     )
     def test_market_capitalization(self, helpers, security_code, market_capitalization):
-        text = helpers.html2text(f"/workspaces/kabupy/tests/kabuyoho/html/{security_code}.html")
+        text = helpers.html2text(
+            filename=os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                f"html/{security_code}.html",
+            )
+        )
         with requests_mock.Mocker() as m:
             m.get(f"https://kabuyoho.jp/sp/reportTop?bcode={security_code}", text=text)
             assert kabupy.kabuyoho.stock(security_code).market_capitalization == Money(market_capitalization, "JPY")
