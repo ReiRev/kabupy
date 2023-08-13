9e5bf6a254b0012ee65fe2043fa3deb9a8e523b8
ReiRev <reirev2913@gmail.com>
2023-08-13 16:43:31 +0000

    impl: actual_pbr

--- src/kabupy/kabuyoho/kabuyoho.py
+++ src/kabupy/kabuyoho/kabuyoho.py
@@ -100,6 +100,14 @@ class Stock:
         return str2float(amount)
 
     @property
+    def actual_pbr(self) -> float | None:
+        """Expected PER: PER(予)."""
+        amount = self.term2description(self.report_top_page, "PBR(実)")
+        if amount is None:
+            return None
+        return str2float(amount)
+
+    @property
     def market_capitalization(self) -> Money | None:
         """Market Capitalization(時価総額)"""
         amount = self.term2description(self.report_top_page, "時価総額")
