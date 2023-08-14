84187c0df5d5271773a3cc2a7b703427f2780eb8
ReiRev <reirev2913@gmail.com>
2023-08-14 01:06:37 +0000

    refactor: change variable name to expected

--- tests/kabuyoho/test_report_target.py
+++ tests/kabuyoho/test_report_target.py
@@ -11,10 +11,10 @@ url_directory = "reportTarget"
 
 class TestReportTarget:
     @pytest.mark.parametrize(
-        "security_code,per_based_theoretical_stock_price",
+        "security_code,expected",
         [(6758, Money("13438", "JPY")), (7837, None)],
     )
-    def test_per_based_theoretical_stock_price(self, helpers, security_code, per_based_theoretical_stock_price):
+    def test_per_based_theoretical_stock_price(self, helpers, security_code, expected):
         text = helpers.html2text(
             filename=os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
@@ -23,16 +23,13 @@ class TestReportTarget:
         )
         with requests_mock.Mocker() as m:
             m.get(f"https://kabuyoho.jp/sp/reportTarget?bcode={security_code}", text=text)
-            assert (
-                kabupy.kabuyoho.stock(security_code).per_based_theoretical_stock_price
-                == per_based_theoretical_stock_price
-            )
+            assert kabupy.kabuyoho.stock(security_code).per_based_theoretical_stock_price == expected
 
     @pytest.mark.parametrize(
-        "security_code,per_based_upside_target",
+        "security_code,expected",
         [(6758, Money("14424", "JPY")), (7837, None)],
     )
-    def test_per_based_upside_target(self, helpers, security_code, per_based_upside_target):
+    def test_per_based_upside_target(self, helpers, security_code, expected):
         text = helpers.html2text(
             filename=os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
@@ -41,13 +38,13 @@ class TestReportTarget:
         )
         with requests_mock.Mocker() as m:
             m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}", text=text)
-            assert kabupy.kabuyoho.stock(security_code).per_based_upside_target == per_based_upside_target
+            assert kabupy.kabuyoho.stock(security_code).per_based_upside_target == expected
 
     @pytest.mark.parametrize(
-        "security_code,per_based_downside_target",
+        "security_code,expected",
         [(6758, Money("12451", "JPY")), (7837, None)],
     )
-    def test_per_based_downside_target(self, helpers, security_code, per_based_downside_target):
+    def test_per_based_downside_target(self, helpers, security_code, expected):
         text = helpers.html2text(
             filename=os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
@@ -56,13 +53,13 @@ class TestReportTarget:
         )
         with requests_mock.Mocker() as m:
             m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}", text=text)
-            assert kabupy.kabuyoho.stock(security_code).per_based_downside_target == per_based_downside_target
+            assert kabupy.kabuyoho.stock(security_code).per_based_downside_target == expected
 
     @pytest.mark.parametrize(
-        "security_code,pbr_based_theoretical_stock_price",
+        "security_code,expected",
         [(6758, Money("12797", "JPY")), (7837, Money("438", "JPY"))],
     )
-    def test_pbr_based_theoretical_stock_price(self, helpers, security_code, pbr_based_theoretical_stock_price):
+    def test_pbr_based_theoretical_stock_price(self, helpers, security_code, expected):
         text = helpers.html2text(
             filename=os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
@@ -71,16 +68,13 @@ class TestReportTarget:
         )
         with requests_mock.Mocker() as m:
             m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}", text=text)
-            assert (
-                kabupy.kabuyoho.stock(security_code).pbr_based_theoretical_stock_price
-                == pbr_based_theoretical_stock_price
-            )
+            assert kabupy.kabuyoho.stock(security_code).pbr_based_theoretical_stock_price == expected
 
     @pytest.mark.parametrize(
-        "security_code,pbr_based_upside_target",
+        "security_code,expected",
         [(6758, Money("13542", "JPY")), (7837, Money("624", "JPY"))],
     )
-    def test_pbr_based_upside_target(self, helpers, security_code, pbr_based_upside_target):
+    def test_pbr_based_upside_target(self, helpers, security_code, expected):
         text = helpers.html2text(
             filename=os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
@@ -89,13 +83,13 @@ class TestReportTarget:
         )
         with requests_mock.Mocker() as m:
             m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}", text=text)
-            assert kabupy.kabuyoho.stock(security_code).pbr_based_upside_target == pbr_based_upside_target
+            assert kabupy.kabuyoho.stock(security_code).pbr_based_upside_target == expected
 
     @pytest.mark.parametrize(
-        "security_code,pbr_based_downside_target",
+        "security_code,expected",
         [(6758, Money("12053", "JPY")), (7837, Money("253", "JPY"))],
     )
-    def test_pbr_based_downside_target(self, helpers, security_code, pbr_based_downside_target):
+    def test_pbr_based_downside_target(self, helpers, security_code, expected):
         text = helpers.html2text(
             filename=os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
@@ -104,13 +98,13 @@ class TestReportTarget:
         )
         with requests_mock.Mocker() as m:
             m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}", text=text)
-            assert kabupy.kabuyoho.stock(security_code).pbr_based_downside_target == pbr_based_downside_target
+            assert kabupy.kabuyoho.stock(security_code).pbr_based_downside_target == expected
 
     @pytest.mark.parametrize(
-        "security_code,price_target",
+        "security_code,expected",
         [(6758, Money("16197", "JPY")), (7837, None)],
     )
-    def test_price_target(self, helpers, security_code, price_target):
+    def test_price_target(self, helpers, security_code, expected):
         text = helpers.html2text(
             filename=os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
@@ -119,4 +113,4 @@ class TestReportTarget:
         )
         with requests_mock.Mocker() as m:
             m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}", text=text)
-            assert kabupy.kabuyoho.stock(security_code).price_target == price_target
+            assert kabupy.kabuyoho.stock(security_code).price_target == expected
