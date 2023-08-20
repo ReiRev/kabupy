9f46c64af4fe038c489124657422f88294bd4100
ReiRev <reirev2913@gmail.com>
2023-08-20 08:09:24 +0000

    add webpage_property

--- src/kabupy/base/__init__.py
+++ src/kabupy/base/__init__.py
@@ -3,5 +3,6 @@ from __future__ import annotations
 
 from .webpage import Webpage
 from .website import Website
+from .decorators import webpage_property
 
-__all__ = ["Website", "Webpage"]
+__all__ = ["Website", "Webpage", "webpage_property"]
