837dc5fef06725e95d6e60bcc5b3e48bfdc10abe
ReiRev <reirev2913@gmail.com>
2023-08-23 15:05:57 +0000

    fix: failed merge

--- src/kabupy/kabuyoho/kabuyoho.py
+++ src/kabupy/kabuyoho/kabuyoho.py
@@ -5,7 +5,6 @@ import functools
 import logging
 
 from ..base import Website
-
 from .report_dps import ReportDps
 from .report_target import ReportTarget
 from .report_top import ReportTop
@@ -41,3 +40,4 @@ class Stock:
 
     @functools.cached_property
     def report_dps(self) -> ReportDps:
+        return ReportDps(self.website, self.security_code)
