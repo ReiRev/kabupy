bc777503ecdcf1ce798c96d424149b362b2e5144
ReiRev <99321158+ReiRev@users.noreply.github.com>
2023-08-27 10:50:44 +0900

    11 implement reporttop (#19)
    * 12 make kabuyohos properties properties of each webpage class (#13)
    
    * change Page class to Webpage class
    
    * format
    
    * fix typo
    
    * initial commit
    
    * initial commit
    
    * add webpage_property
    
    * separate properties
    
    * 14 make test code simpler (#15)
    
    * fix class type
    
    * combine tests
    
    * impl: name
    
    * impl: security_code
    
    * impl: earnings_release_date
    
    * Update README.md
    
    * Update README.md
    
    * impl: expected_dividend_yield
    
    * fix: reorder properties
    
    * impl: target_price
    
    * impl: business_category
    
    * fix: remove an unnecessary line
    
    * impl: products
    
    * impl: segment_sales_composition
    
    * impl: income_statement_amount
    
    * impl: income_statement_adjustment
    
    * impl: current_term_company_performance_forecast
    
    * impl: analyst_company_performance_forecast_comparison
    
    * impl: price_level_to_target
    
    * impl: price_level_to_pbr_based_theoretical_price
    
    * impl: price_level_to_per_based_theoretical_price
    
    * impl: risk_on_relative_index
    
    * impl: news_links

--- src/kabupy/kabuyoho/kabuyoho_webpage.py
+++ src/kabupy/kabuyoho/kabuyoho_webpage.py
@@ -1,6 +1,9 @@
 """Base class for webpage"""
 from __future__ import annotations
 
+import re
+from datetime import datetime
+
 from money import Money
 
 from ..base import Webpage, webpage_property
@@ -9,6 +12,8 @@ from ..util import str2money
 
 class KabuyohoWebpage(Webpage):
     """Base class for kabuyoho webpage"""
+
+    security_code: str
 
     def term2description(self, term: str) -> str | None:
         res = self.soup.select_one(f'main dt:-soup-contains("{term}") + dd')
@@ -23,3 +28,23 @@ class KabuyohoWebpage(Webpage):
         if amount is None:
             return None
         return str2money(amount.text)
+
+    @webpage_property
+    def name(self) -> str | None:
+        """Name of the stock: 銘柄名"""
+        res = self.soup.select_one(f"main ul:-soup-contains('{self.security_code}') > li")
+        if res is None:
+            return None
+        return res.text
+
+    @webpage_property
+    def earnings_release_date(self) -> datetime | None:
+        """Earnings release date: 決算発表日"""
+        res = self.soup.select_one(f"main ul:-soup-contains('{self.security_code}') > li:last-of-type")
+        if res is None:
+            return None
+        match = re.search(r"(\d{4})/(\d{2})/(\d{2})", res.text)
+        if match:
+            year, month, day = match.groups()
+            return datetime(int(year), int(month), int(day))
+        return None
