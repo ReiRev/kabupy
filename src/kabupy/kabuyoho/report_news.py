a1d11716af6038880cef4d9239b7702343674720
ReiRev <reirev2913@gmail.com>
2023-09-03 14:29:34 +0900

    refactor

--- src/kabupy/kabuyoho/report_news.py
+++ src/kabupy/kabuyoho/report_news.py
@@ -7,8 +7,12 @@ import re
 import urllib.parse
 from datetime import datetime
 
-from ..base import Website, webpage_property
+import time
+
+from ..base import Website
+from ..errors import ElementNotFoundError
 from .kabuyoho_webpage import KabuyohoWebpage
+from ..constatns import TIME_SLEEP
 
 logger = logging.getLogger(__name__)
 
@@ -24,26 +28,51 @@ class ReportNews(KabuyohoWebpage):
         super().__init__(load=False)
 
     @functools.cached_property
-    def flash_report(self) -> FlashReport:
-        """Flash report page in a report news page."""
-        return FlashReport(self.website, self.security_code)
+    def market_report(self) -> KabuyohoNewsWebpage:
+        """Market report page in a report news page."""
+        return KabuyohoNewsWebpage(self.website, self.security_code, 1)
 
     @functools.cached_property
-    def market_report(self) -> MarketReport:
-        """Market report page in a report news page."""
-        return MarketReport(self.website, self.security_code)
+    def flash_report(self) -> KabuyohoNewsWebpage:
+        """Flash report page in a report news page."""
+        return KabuyohoNewsWebpage(self.website, self.security_code, 2)
+
+    @functools.cached_property
+    def analyst_prediction(self) -> KabuyohoNewsWebpage:
+        """Analyst prediction page in a report news page."""
+        return KabuyohoNewsWebpage(self.website, self.security_code, 3)
+
+    @functools.cached_property
+    def analyst_evaluation(self) -> KabuyohoNewsWebpage:
+        """Analyst evaluation page in a report news page."""
+        return KabuyohoNewsWebpage(self.website, self.security_code, 4)
 
 
 class KabuyohoNewsWebpage(KabuyohoWebpage):
     """Kabuyoho news page object."""
 
-    website: Website
+    def __init__(self, website: Website, security_code: str | int, category: int) -> None:
+        self.website = website
+        self.security_code = str(security_code)
+        self.category = category
+        self.url = urllib.parse.urljoin(
+            self.website.url, f"sp/reportNews?bcode={self.security_code}&cat={self.category}"
+        )
+        super().__init__()
 
-    def get_links(self) -> list[dict]:
+    def get_max_page(self) -> int:
+        """Max page number."""
+        try:
+            page = self.select_one("div.pager > ul > li.interval + li")
+        except ElementNotFoundError:
+            return 1
+        return int(re.sub(r"[\D]", "", page.text))
+
+    def get_links(self, max_page: int | None = 1, time_sleep: float = TIME_SLEEP) -> list[dict]:
         """list of links.
 
         Args:
-            category (str): Category of news.
+            max_page (int | None, optional): Max page number. Defaults to 1. If None, all pages are scraped.
 
         Returns:
             list[dict]: List of news.
@@ -61,90 +90,37 @@ class KabuyohoNewsWebpage(KabuyohoWebpage):
                 ...
             ]
         """
-        dates = self.select("div.sp_news_list > ul span.time")
-        dates = [datetime.strptime(re.sub(r"[\D]", "", d.text), "%Y%m%d%H%M") for d in dates]
-        titles = self.select("div.sp_news_list > ul p.list_title")
-        titles = [t.text for t in titles]
-        categories = self.select("div.sp_news_list > ul span.ctgr")
-        categories = [c.text for c in categories]
-        weathers = self.select("div.sp_news_list > ul span.wthr")
-        weathers = [w.get("class") for w in weathers]
-        for i, weather in enumerate(weathers):
-            if weather is None:
-                weathers[i] = None
-            elif isinstance(weather, list):
-                _extracted = [w for w in weather if w != "wthr"]
-                weathers[i] = _extracted[0] if len(_extracted) > 0 else None
-            elif isinstance(weather, str):
-                weathers[i] = weather if weather != "wthr" else None
-        urls = self.select("div.sp_news_list > ul a")
-        urls = [u.get("href") for u in urls]
-        urls = [urllib.parse.urljoin(self.website.url, u) for u in urls if isinstance(u, str)]
-        return [
-            {"date": date, "title": title, "category": category, "weather": weather, "url": url}
-            for date, title, category, weather, url in zip(dates, titles, categories, weathers, urls)
-        ]
-
-
-class MarketReport(KabuyohoNewsWebpage):
-    """Market report page in a report news page."""
-
-    def __init__(self, website: Website, security_code: str | int) -> None:
-        self.website = website
-        self.security_code = str(security_code)
-        self.url = urllib.parse.urljoin(self.website.url, f"sp/reportNews?bcode={self.security_code}&cat=1")
-        super().__init__()
-
-    @webpage_property
-    def links(self) -> list[dict]:
-        """list of market reports(マーケット).
-
-        Returns:
-            list[dict]: List of news.
-
-        Note:
-            The example of the return value is as follows:
-            [
-                {
-                    "date": datetime(2021, 3, 1, 12, 34),
-                    "title": "FooBar",
-                    "category": "外国市場/為替",
-                    "weather": None,
-                    "url": "https://kabuyoho.jp/sp/example"
-                },
-                ...
+        res = []
+        if max_page is None:
+            max_page = self.get_max_page()
+        else:
+            max_page = min(max_page, self.get_max_page())
+        for p in range(1, max_page + 1):
+            if p > 1:
+                time.sleep(time_sleep)
+                self.url = self.url + f"&page={p}"
+                self.load()
+            dates = self.select("div.sp_news_list > ul span.time")
+            dates = [datetime.strptime(re.sub(r"[\D]", "", d.text), "%Y%m%d%H%M") for d in dates]
+            titles = self.select("div.sp_news_list > ul p.list_title")
+            titles = [t.text for t in titles]
+            categories = self.select("div.sp_news_list > ul span.ctgr")
+            categories = [c.text for c in categories]
+            weathers = self.select("div.sp_news_list > ul span.wthr")
+            weathers = [w.get("class") for w in weathers]
+            for i, weather in enumerate(weathers):
+                if weather is None:
+                    weathers[i] = None
+                elif isinstance(weather, list):
+                    _extracted = [w for w in weather if w != "wthr"]
+                    weathers[i] = _extracted[0] if len(_extracted) > 0 else None
+                elif isinstance(weather, str):
+                    weathers[i] = weather if weather != "wthr" else None
+            urls = self.select("div.sp_news_list > ul a")
+            urls = [u.get("href") for u in urls]
+            urls = [urllib.parse.urljoin(self.website.url, u) for u in urls if isinstance(u, str)]
+            res = res + [
+                {"date": date, "title": title, "category": category, "weather": weather, "url": url}
+                for date, title, category, weather, url in zip(dates, titles, categories, weathers, urls)
             ]
-        """
-        return self.get_links()
-
-
-class FlashReport(KabuyohoNewsWebpage):
-    """Flash report page in a report news page."""
-
-    def __init__(self, website: Website, security_code: str | int) -> None:
-        self.website = website
-        self.security_code = str(security_code)
-        self.url = urllib.parse.urljoin(self.website.url, f"sp/reportNews?bcode={self.security_code}&cat=2")
-        super().__init__()
-
-    @webpage_property
-    def links(self) -> list[dict]:
-        """list of flash reports(業績速報).
-
-        Returns:
-            list[dict]: List of news.
-
-        Note:
-            The example of the return value is as follows:
-            [
-                {
-                    "date": datetime(2021, 3, 1, 12, 34),
-                    "title": "FooBar",
-                    "category": "決算",
-                    "weather": "wthr_clud",
-                    "url": "https://kabuyoho.jp/sp/example"
-                },
-                ...
-            ]
-        """
-        return self.get_links()
+        return res
--- tests/kabuyoho/test_report_news.py
+++ tests/kabuyoho/test_report_news.py
@@ -79,7 +79,92 @@ class TestReportNews:
         )
         with requests_mock.Mocker() as m:
             m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}&cat=1", text=text)
-            assert kabupy.kabuyoho.stock(security_code).report_news.market_report.links == expected_values
+            assert kabupy.kabuyoho.stock(security_code).report_news.market_report.get_links() == expected_values
+
+    @pytest.mark.parametrize(
+        "security_code,expected_values",
+        [
+            (
+                6758,
+                [
+                    {
+                        "date": datetime(2023, 9, 2, 9, 22),
+                        "title": "1日のNY市場はまちまち",
+                        "category": "外国市場/為替",
+                        "weather": None,
+                        "url": "https://kabuyoho.jp/sp/fiscoNewsDetail?nid=0005330020230902001&bcode=6758",
+                    },
+                    {
+                        "date": datetime(2023, 9, 2, 9, 20),
+                        "title": "米国株式市場はまちまち、経済指標は強弱混在（1日）",
+                        "category": "外国市場/為替",
+                        "weather": None,
+                        "url": "https://kabuyoho.jp/sp/fiscoNewsDetail?nid=0000050020230902001&bcode=6758",
+                    },
+                    {
+                        "date": datetime(2023, 9, 2, 7, 15),
+                        "title": "前日に動いた銘柄 part1　内田洋行、JDSC、四電工など",
+                        "category": "銘柄/投資戦略",
+                        "weather": None,
+                        "url": "https://kabuyoho.jp/sp/fiscoNewsDetail?nid=0009350020230902001&bcode=6758",
+                    },
+                    {
+                        "date": datetime(2023, 9, 2, 9, 22),
+                        "title": "1日のNY市場はまちまち",
+                        "category": "外国市場/為替",
+                        "weather": None,
+                        "url": "https://kabuyoho.jp/sp/fiscoNewsDetail?nid=0005330020230902001&bcode=6758",
+                    },
+                    {
+                        "date": datetime(2023, 9, 2, 9, 20),
+                        "title": "米国株式市場はまちまち、経済指標は強弱混在（1日）",
+                        "category": "外国市場/為替",
+                        "weather": None,
+                        "url": "https://kabuyoho.jp/sp/fiscoNewsDetail?nid=0000050020230902001&bcode=6758",
+                    },
+                    {
+                        "date": datetime(2023, 9, 2, 7, 15),
+                        "title": "前日に動いた銘柄 part1　内田洋行、JDSC、四電工など",
+                        "category": "銘柄/投資戦略",
+                        "weather": None,
+                        "url": "https://kabuyoho.jp/sp/fiscoNewsDetail?nid=0009350020230902001&bcode=6758",
+                    },
+                ],
+            )
+        ],
+    )
+    def test_market_report_double_page(self, helpers, security_code, expected_values: dict):
+        text = helpers.html2text(
+            filename=os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                f"html/{url_directory}/market_report/{security_code}.html",
+            )
+        )
+        with requests_mock.Mocker() as m:
+            m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}&cat=1", text=text)
+            m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}&cat=1&page=2", text=text)
+            assert (
+                kabupy.kabuyoho.stock(security_code).report_news.market_report.get_links(max_page=2, time_sleep=0)
+                == expected_values
+            )
+
+    @pytest.mark.parametrize(
+        "security_code,expected_values",
+        [
+            (6758, 69),
+            (7837, 1),
+        ],
+    )
+    def test_max_page(self, helpers, security_code, expected_values: dict):
+        text = helpers.html2text(
+            filename=os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                f"html/{url_directory}/market_report/{security_code}.html",
+            )
+        )
+        with requests_mock.Mocker() as m:
+            m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}&cat=1", text=text)
+            assert kabupy.kabuyoho.stock(security_code).report_news.market_report.get_max_page() == expected_values
 
     @pytest.mark.parametrize(
         "security_code,expected_values",
@@ -153,7 +238,7 @@ class TestReportNews:
         )
         with requests_mock.Mocker() as m:
             m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}&cat=2", text=text)
-            assert kabupy.kabuyoho.stock(security_code).report_news.flash_report.links == expected_values
+            assert kabupy.kabuyoho.stock(security_code).report_news.flash_report.get_links() == expected_values
 
     def test_flash_report_raise_element_not_found_error(self, helpers):
         text = helpers.html2text(
@@ -165,4 +250,4 @@ class TestReportNews:
         with requests_mock.Mocker() as m:
             m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode=6758&cat=2", text=text)
             with pytest.raises(ElementNotFoundError):
-                kabupy.kabuyoho.stock(6758).report_news.flash_report.links
+                kabupy.kabuyoho.stock(6758).report_news.flash_report.get_links()
