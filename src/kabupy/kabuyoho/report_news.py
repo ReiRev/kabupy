dc8b2a1d79a90807d3ef54134397b69149d3d43b
ReiRev <reirev2913@gmail.com>
2023-09-03 12:57:08 +0900

    implemented market report and flash report

--- src/kabupy/kabuyoho/report_news.py
+++ src/kabupy/kabuyoho/report_news.py
@@ -5,6 +5,7 @@ import logging
 import re
 import urllib.parse
 from datetime import datetime
+import functools
 
 from ..base import Website, webpage_property
 from .kabuyoho_webpage import KabuyohoWebpage
@@ -19,10 +20,85 @@ class ReportNews(KabuyohoWebpage):
         self.website = website
         self.security_code = str(security_code)
         self.url = urllib.parse.urljoin(self.website.url, f"sp/reportNews?bcode={self.security_code}")
+        # No need to call super().__init__() because this class does not have any webpage property.
+        super().__init__(load=False)
+
+    @functools.cached_property
+    def flash_report(self) -> FlashReport:
+        """Flash report page in a report news page."""
+        return FlashReport(self.website, self.security_code)
+
+    @functools.cached_property
+    def market_report(self) -> MarketReport:
+        """Market report page in a report news page."""
+        return MarketReport(self.website, self.security_code)
+
+
+class MarketReport(KabuyohoWebpage):
+    """Market report page in a report news page."""
+
+    def __init__(self, website: Website, security_code: str | int) -> None:
+        self.website = website
+        self.security_code = str(security_code)
+        self.url = urllib.parse.urljoin(self.website.url, f"sp/reportNews?bcode={self.security_code}&cat=1")
         super().__init__()
 
     @webpage_property
-    def flash_reports(self) -> list[dict]:
+    def links(self) -> list[dict]:
+        """list of market reports(マーケット).
+
+        Returns:
+            list[dict]: List of news.
+
+        Note:
+            The example of the return value is as follows:
+            [
+                {
+                    "date": datetime(2021, 3, 1, 12, 34),
+                    "title": "FooBar",
+                    "category": "決算",
+                    "weather": "wthr_clud",
+                    "url": "https://kabuyoho.jp/sp/example"
+                },
+                ...
+            ]
+        """
+        dates = self.select("div.sp_news_list > ul span.time")
+        dates = [datetime.strptime(re.sub(r"[\D]", "", d.text), "%Y%m%d%H%M") for d in dates]
+        titles = self.select("div.sp_news_list > ul p.list_title")
+        titles = [t.text for t in titles]
+        categories = self.select("div.sp_news_list > ul span.ctgr")
+        categories = [c.text for c in categories]
+        weathers = self.select("div.sp_news_list > ul span.wthr")
+        weathers = [w.get("class") for w in weathers]
+        for i, weather in enumerate(weathers):
+            if weather is None:
+                weathers[i] = None
+            elif isinstance(weather, list):
+                _extracted = [w for w in weather if w != "wthr"]
+                weathers[i] = _extracted[0] if len(_extracted) > 0 else None
+            elif isinstance(weather, str):
+                weathers[i] = weather
+        urls = self.select("div.sp_news_list > ul a")
+        urls = [u.get("href") for u in urls]
+        urls = [urllib.parse.urljoin(self.website.url, u) for u in urls if isinstance(u, str)]
+        return [
+            {"date": date, "title": title, "category": category, "weather": weather, "url": url}
+            for date, title, category, weather, url in zip(dates, titles, categories, weathers, urls)
+        ]
+
+
+class FlashReport(KabuyohoWebpage):
+    """Flash report page in a report news page."""
+
+    def __init__(self, website: Website, security_code: str | int) -> None:
+        self.website = website
+        self.security_code = str(security_code)
+        self.url = urllib.parse.urljoin(self.website.url, f"sp/reportNews?bcode={self.security_code}&cat=2")
+        super().__init__()
+
+    @webpage_property
+    def links(self) -> list[dict]:
         """list of flash reports(業績速報).
 
         Returns:
--- tests/kabuyoho/test_report_news.py
+++ tests/kabuyoho/test_report_news.py
@@ -6,7 +6,6 @@ import pytest
 import requests_mock
 
 import kabupy
-from kabupy.base.decorators import webpage_property
 from kabupy.errors import ElementNotFoundError
 
 url_directory = "reportNews"
@@ -19,81 +18,144 @@ class TestReportNews:
         [
             (
                 6758,
-                {
-                    "flash_reports": [
-                        {
-                            "date": datetime(2023, 8, 9, 15, 1),
-                            "title": "2024年3月期連結第1四半期、税引前損益276,034百万円。IFISコンセンサスを上回る水準。",
-                            "category": "決算",
-                            "weather": "wthr_clud",
-                            "url": "https://kabuyoho.jp/sp/"
-                            "consNewsDetail?cat=1&nid=6758_20230809_act_20230809_150126_2&bcode=6758",
-                        },
-                        {
-                            "date": datetime(2023, 4, 28, 15, 2),
-                            "title": "2023年3月期連結、5.6%税引前増益。IFISコンセンサスを上回る水準。",
-                            "category": "決算",
-                            "weather": "wthr_clud",
-                            "url": "https://kabuyoho.jp/sp/"
-                            "consNewsDetail?cat=1&nid=6758_20230428_act_20230428_150217_1&bcode=6758",
-                        },
-                        {
-                            "date": datetime(2023, 2, 2, 15, 0),
-                            "title": "2023年3月期連結第3四半期(累計)、税引前損益1,035,711百万円。",
-                            "category": "決算",
-                            "weather": "wthr_clud",
-                            "url": "https://kabuyoho.jp/sp/"
-                            "consNewsDetail?cat=1&nid=6758_20230202_act_20230202_150027_1&bcode=6758",
-                        },
-                    ],
-                },
+                [
+                    {
+                        "date": datetime(2023, 9, 2, 9, 22),
+                        "title": "1日のNY市場はまちまち",
+                        "category": "外国市場/為替",
+                        "weather": None,
+                        "url": "https://kabuyoho.jp/sp/fiscoNewsDetail?nid=0005330020230902001&bcode=6758",
+                    },
+                    {
+                        "date": datetime(2023, 9, 2, 9, 20),
+                        "title": "米国株式市場はまちまち、経済指標は強弱混在（1日）",
+                        "category": "外国市場/為替",
+                        "weather": None,
+                        "url": "https://kabuyoho.jp/sp/fiscoNewsDetail?nid=0000050020230902001&bcode=6758",
+                    },
+                    {
+                        "date": datetime(2023, 9, 2, 7, 15),
+                        "title": "前日に動いた銘柄 part1　内田洋行、JDSC、四電工など",
+                        "category": "銘柄/投資戦略",
+                        "weather": None,
+                        "url": "https://kabuyoho.jp/sp/fiscoNewsDetail?nid=0009350020230902001&bcode=6758",
+                    },
+                ],
             ),
             (
                 7837,
-                {
-                    "flash_reports": [
-                        {
-                            "date": datetime(2023, 8, 14, 15, 1),
-                            "title": "2024年3月期連結第1四半期、経常損益62百万円。",
-                            "category": "決算",
-                            "weather": "wthr_clud",
-                            "url": "https://kabuyoho.jp/sp/"
-                            "consNewsDetail?cat=1&nid=7837_20230814_act_20230814_150116_1&bcode=7837",
-                        },
-                        {
-                            "date": datetime(2023, 5, 15, 17, 30),
-                            "title": "2023年3月期連結、経常赤字幅拡大。事前予想を下回る水準。",
-                            "category": "決算",
-                            "weather": "wthr_rain",
-                            "url": "https://kabuyoho.jp/sp/"
-                            "consNewsDetail?cat=1&nid=7837_20230515_act_20230515_173006_1&bcode=7837",
-                        },
-                        {
-                            "date": datetime(2023, 2, 14, 16, 0),
-                            "title": "2023年3月期連結第3四半期(累計)、経常損益-591百万円。",
-                            "category": "決算",
-                            "weather": "wthr_clud",
-                            "url": "https://kabuyoho.jp/sp/"
-                            "consNewsDetail?cat=1&nid=7837_20230214_act_20230214_160054_1&bcode=7837",
-                        },
-                    ],
-                },
+                [
+                    {
+                        "date": datetime(2023, 5, 12, 17, 10),
+                        "title": "ブロンコＢなどがランクイン",
+                        "category": "市況・概要",
+                        "weather": None,
+                        "url": "https://kabuyoho.jp/sp/fiscoNewsDetail?nid=0005550020230512001&bcode=7837",
+                    },
+                    {
+                        "date": datetime(2023, 4, 28, 17, 19),
+                        "title": "テラスカイがランクイン",
+                        "category": "市況・概要",
+                        "weather": None,
+                        "url": "https://kabuyoho.jp/sp/fiscoNewsDetail?nid=0005550020230428001&bcode=7837",
+                    },
+                    {
+                        "date": datetime(2023, 4, 14, 17, 29),
+                        "title": "正栄食品工業がランクイン",
+                        "category": "市況・概要",
+                        "weather": None,
+                        "url": "https://kabuyoho.jp/sp/fiscoNewsDetail?nid=0005550020230414001&bcode=7837",
+                    },
+                ],
             ),
         ],
     )
-    def test_properties(self, helpers, security_code, expected_values: dict):
+    def test_market_report_single_page(self, helpers, security_code, expected_values: dict):
         text = helpers.html2text(
             filename=os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
-                f"html/{url_directory}/{security_code}.html",
+                f"html/{url_directory}/market_report/{security_code}.html",
             )
         )
         with requests_mock.Mocker() as m:
-            m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}", text=text)
-            for k, v in expected_values.items():
-                assert getattr(kabupy.kabuyoho.stock(security_code).report_news, k) == v
+            m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}&cat=1", text=text)
+            assert kabupy.kabuyoho.stock(security_code).report_news.market_report.links == expected_values
 
-    def test_raise_element_not_found_error(self, helpers):
+    @pytest.mark.parametrize(
+        "security_code,expected_values",
+        [
+            (
+                6758,
+                [
+                    {
+                        "date": datetime(2023, 8, 9, 15, 1),
+                        "title": "2024年3月期連結第1四半期、税引前損益276,034百万円。IFISコンセンサスを上回る水準。",
+                        "category": "決算",
+                        "weather": "wthr_clud",
+                        "url": "https://kabuyoho.jp/sp/"
+                        "consNewsDetail?cat=1&nid=6758_20230809_act_20230809_150126_2&bcode=6758",
+                    },
+                    {
+                        "date": datetime(2023, 4, 28, 15, 2),
+                        "title": "2023年3月期連結、5.6%税引前増益。IFISコンセンサスを上回る水準。",
+                        "category": "決算",
+                        "weather": "wthr_clud",
+                        "url": "https://kabuyoho.jp/sp/"
+                        "consNewsDetail?cat=1&nid=6758_20230428_act_20230428_150217_1&bcode=6758",
+                    },
+                    {
+                        "date": datetime(2023, 2, 2, 15, 0),
+                        "title": "2023年3月期連結第3四半期(累計)、税引前損益1,035,711百万円。",
+                        "category": "決算",
+                        "weather": "wthr_clud",
+                        "url": "https://kabuyoho.jp/sp/"
+                        "consNewsDetail?cat=1&nid=6758_20230202_act_20230202_150027_1&bcode=6758",
+                    },
+                ],
+            ),
+            (
+                7837,
+                [
+                    {
+                        "date": datetime(2023, 8, 14, 15, 1),
+                        "title": "2024年3月期連結第1四半期、経常損益62百万円。",
+                        "category": "決算",
+                        "weather": "wthr_clud",
+                        "url": "https://kabuyoho.jp/sp/"
+                        "consNewsDetail?cat=1&nid=7837_20230814_act_20230814_150116_1&bcode=7837",
+                    },
+                    {
+                        "date": datetime(2023, 5, 15, 17, 30),
+                        "title": "2023年3月期連結、経常赤字幅拡大。事前予想を下回る水準。",
+                        "category": "決算",
+                        "weather": "wthr_rain",
+                        "url": "https://kabuyoho.jp/sp/"
+                        "consNewsDetail?cat=1&nid=7837_20230515_act_20230515_173006_1&bcode=7837",
+                    },
+                    {
+                        "date": datetime(2023, 2, 14, 16, 0),
+                        "title": "2023年3月期連結第3四半期(累計)、経常損益-591百万円。",
+                        "category": "決算",
+                        "weather": "wthr_clud",
+                        "url": "https://kabuyoho.jp/sp/"
+                        "consNewsDetail?cat=1&nid=7837_20230214_act_20230214_160054_1&bcode=7837",
+                    },
+                ],
+            ),
+        ],
+    )
+    def test_flash_report(self, helpers, security_code, expected_values: dict):
+        text = helpers.html2text(
+            filename=os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                f"html/{url_directory}/flash_report/{security_code}.html",
+            )
+        )
+        with requests_mock.Mocker() as m:
+            m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode={security_code}&cat=2", text=text)
+            assert kabupy.kabuyoho.stock(security_code).report_news.flash_report.links == expected_values
+
+    def test_flash_report_raise_element_not_found_error(self, helpers):
         text = helpers.html2text(
             filename=os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
@@ -101,10 +163,6 @@ class TestReportNews:
             )
         )
         with requests_mock.Mocker() as m:
-            m.get("https://kabuyoho.jp/sp/reportNews?bcode=6758", text=text)
-            page = kabupy.kabuyoho.stock(6758).report_news
-            for k, v in page.__class__.__dict__.items():
-                if isinstance(v, webpage_property):
-                    logger.debug(f"Testing {k}")
-                    with pytest.raises(ElementNotFoundError):
-                        getattr(page, k)
+            m.get(f"https://kabuyoho.jp/sp/{url_directory}?bcode=6758&cat=2", text=text)
+            with pytest.raises(ElementNotFoundError):
+                kabupy.kabuyoho.stock(6758).report_news.flash_report.links
